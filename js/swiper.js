2fc72c24ded44b2e57541fc2dcda9a7076f2d92d
NickButnar <103021759+NickButnar@users.noreply.github.com>
2022-11-29 23:14:06 +0400

    Последнии исправления. Адаптация под любые размеры экранов. Слайдер в разработке.

--- js/swiper.js
+++ js/swiper.js
@@ -1,9 +1,36 @@
 const swiper = document.querySelector(".swiper");
 const swiperItems = Array.from(swiper.children);
+const btnNext = document.querySelector("#slider__next");
 
-swiperItems.forEach(function (swiper__slide) {
-  console.log(swiper__slide);
-  swiper__slide.classlist.add("hidden");
+swiperItems.forEach(function (swiper__slide, index) {
+  if (index !== 0) {
+    swiper__slide.classList.add("hidden");
+  }
 
-  // Uncaught TypeError: Cannot read properties of undefined (reading 'add')
+  swiper__slide.dataset.index = index;
+
+  swiperItems[0].setAttribute("data-active", "");
+
+  // click
+
+  swiper__slide.addEventListener("click", function () {
+    swiper__slide.classList.add("hidden");
+    swiper__slide.removeAttribute("data-active");
+
+    let nextSlideIndex;
+    if (index + 1 === swiperItems.length) {
+      nextSlideIndex = 0;
+    } else {
+      nextSlideIndex = index + 1;
+    }
+
+    const nextSlide = swiper.querySelector(`[data-index="${nextSlideIndex}"]`);
+    nextSlide.classList.remove("hidden");
+    nextSlide.setAttribute("data-active", "");
+  });
 });
+
+btnNext.onclick = function () {
+  const currentSlide = swiper.querySelector("[data-active]");
+  console.log(currentSlide);
+};
