f85ef7fcb0f62cf8b93ed0268f0980e887e548d2
NickButnar <103021759+NickButnar@users.noreply.github.com>
2022-12-01 16:05:42 +0400

    Готовый проект

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+  $(".slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 2000,
+  });
+});
